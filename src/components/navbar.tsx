f6427176497bc570148b4cb42fa18e5538c8157c
Younes Bouhouche <younes.bouhouche12@gmail.com>
2025-04-14 15:59:39 +0100

    Fixed header and footer content alignment

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,7 +13,7 @@ import { MobileNav } from "@/components/mobile-nav"
 
 export function Navbar() {
   return (
-    <header className="border-b sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center">
+    <header className="border-b sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-6">
           <Link href="/" className="flex items-center space-x-2">
